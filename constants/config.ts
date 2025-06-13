06e40c98afcef1cfd96abe583a20d295922008a9
kyyril <khairilrahman55@gmail.com>
2025-06-13 16:49:28 +0700

    feat: Configure environment variables for development and production

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL! || "http://localhost:5000",
+  apiBaseUrl: process.env.NEXT_PUBLIC_API_URL! || "http://localhost:5000",
   googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID! || "",
 };
